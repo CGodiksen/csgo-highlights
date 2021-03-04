b430691a7d3a6e91fa595690fc661df859b533ed
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-04 12:42:32 +0100

    Priting kills and event object for demo file.

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
-console.log("Hello world");
+import { getHighlightSpecification } from "./services/highlights";
+
+getHighlightSpecification();
--- services/highlights.ts
+++ services/highlights.ts
@@ -3,28 +3,31 @@
 // TODO: Combine moments that are within 30 seconds of eachother (avoid too many cuts) and add 5 seconds before the first moment (maybe also after the last).
 // TODO: If the bomb is planted but no kills happen the highlight video should show the bomb blowing up.
 // TODO: Remove irrelevant moments (eco-rounds)
+import fs = require("fs");
+import demofile = require("demofile");
 
-import fs from "fs";
-import demofile from "demofile";
-
-fs.readFile("test.dem", (_err, buffer) => {
-  const demoFile = new demofile.DemoFile();
-
-  demoFile.gameEvents.on("player_death", e => {
-    console.log(e);
+const getHighlightSpecification = (): void => {
+    fs.readFile("data/demos/gambit-vs-virtus-pro-m1-vertigo.dem", (_err, buffer) => {
+      const demoFile = new demofile.DemoFile();
     
-    const victim = demoFile.entities.getByUserId(e.userid);
-    const victimName = victim ? victim.name : "unnamed";
-
-    // Attacker may have disconnected so be aware. e.g. attacker could have thrown a grenade, disconnected, 
-    // then that grenade killed another player.
-    const attacker = demoFile.entities.getByUserId(e.attacker);
-    const attackerName = attacker ? attacker.name : "unnamed";
-
-    const headshotText = e.headshot ? " HS" : "";
+      demoFile.gameEvents.on("player_death", e => {
+        console.log(e);
+        
+        const victim = demoFile.entities.getByUserId(e.userid);
+        const victimName = victim ? victim.name : "unnamed";
     
-    console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName}`);
-  });
+        // Attacker may have disconnected so be aware. e.g. attacker could have thrown a grenade, disconnected, 
+        // then that grenade killed another player.
+        const attacker = demoFile.entities.getByUserId(e.attacker);
+        const attackerName = attacker ? attacker.name : "unnamed";
+    
+        const headshotText = e.headshot ? " HS" : "";
+        
+        console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName}`);
+      });
+    
+      demoFile.parse(buffer);
+    });
+};
 
-  demoFile.parse(buffer);
-});
+export { getHighlightSpecification };
