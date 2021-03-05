a6e7a66f22b441d2ab0e367622094c28b3de3db7
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-05 16:42:58 +0100

    Added function that prints all upcoming relevant matches within a certain time period.

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 // import fs = require("fs");
-//import { getHighlights, getDuration } from "./services/highlighter";
+// import { getHighlights, getDuration } from "./services/highlighter";
 import { addUpcomingMatches } from "./services/scheduler";
 
 
-addUpcomingMatches();
+addUpcomingMatches(5);
 // const demo = fs.readFileSync("data/demos/gambit-vs-virtus-pro-m1-vertigo.dem");
 // void getDuration(demo).then(duration => console.log(duration));
 // void getHighlights(demo).then(highlights => console.log(highlights));
--- services/scheduler.ts
+++ services/scheduler.ts
@@ -3,9 +3,13 @@
 // TODO: The function should use HLTV.getMatches and save the ID and date to a JSON file. 
 import HLTV from 'hltv';
 
-const addUpcomingMatches = (): void => {
+const addUpcomingMatches = (dayLookhead: number): void => {
     void HLTV.getMatches().then(res => {
-        console.log(res);
+        // The limit for how far we look ahead when parsing upcoming matches.
+        const epochLimit = Date.now() + (dayLookhead * 24 * 60 * 60 * 1000);
+        
+        const relevantMatches = res.filter(match => !match.live && match.date && match.date < epochLimit && match.stars > 0);
+        console.log(relevantMatches);
     });
 };
 
