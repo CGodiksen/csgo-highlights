458e3157f5e75052b5eddade67df179273efec1d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-05 16:07:42 +0100

    Added scheduler.

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import fs = require("fs");
-import { getHighlights } from "./services/highlights";
+import { getHighlights, getDuration } from "./services/highlights";
 
 const demo = fs.readFileSync("data/demos/gambit-vs-virtus-pro-m1-vertigo.dem");
 
-// void getDuration(demo).then(duration => console.log(duration));
+void getDuration(demo).then(duration => console.log(duration));
 void getHighlights(demo).then(highlights => console.log(highlights));
---
+++ services/scheduler.ts
@@ -0,0 +1,3 @@
+// TODO: Add a function that looks at upcoming matches for a given time period and saves all matches that should be processed.
+// It should only add matches that have atleast one star.
+// TODO: The function should use HLTV.getMatches and save the ID and date to a JSON file. 
