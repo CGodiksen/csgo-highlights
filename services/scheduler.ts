00835f3097922f6e7a00f808c246ba7c03ba8b34
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-05 23:44:38 +0100

    Added function to return the matches currently in the live.json file.

--- services/scheduler.ts
+++ services/scheduler.ts
@@ -1,22 +1,45 @@
 // TODO: Add a function that starts all the different scheduled jobs which can be called from index once the program starts.
-// TODO: Once every hour the upcoming matches should be checked and if any of the games are live they should be moved to live game file. 
-// TODO: Once every 1 min the other file should have it matches checked to see if they are done and can be processed. 
+// TODO: Once every 45 minutes a function should check if there are any new live games. 
+// TODO: Once every 10 min a function should check if the live games are finished and ready to be processed.
 // If so this part of the scheduler should start the processing phase from download to upload.
 // This could perhaps also be done with jobs that are scheduled for specific times but that is less persistant and its hard to predict when a game is done.
 import HLTV from 'hltv';
 import fs = require("fs");
+import { LiveMatch } from 'hltv/lib/models/LiveMatch';
 
-const addUpcomingMatches = (dayLookhead: number): void => {
+const addLiveMatches = (): void => {
     void HLTV.getMatches().then(res => {
-        // The limit for how far we look ahead when parsing upcoming matches.
-        const epochLimit = Date.now() + (dayLookhead * 24 * 60 * 60 * 1000);
+        const file_path = "./data/scheduler/live.json";
+        if (!fs.existsSync(file_path)) {
+            fs.writeFile("./data/scheduler/live.json", JSON.stringify([]), err => {
+                if (err) throw err;
+            });
+        }
+        else {
+            const previousLiveMatches: unknown = JSON.parse(fs.readFileSync(file_path, "utf-8"));
+        }
 
-        const relevantMatches = res.filter(match => !match.live && match.date && match.date < epochLimit && match.stars > 0);
-        saveUpcomingMatches(JSON.stringify(relevantMatches));
+        const relevantLiveMatches = res.filter(match => match.live && match.stars > 0);
+
+
+        saveLiveMatches(JSON.stringify(relevantLiveMatches));
     });
 };
 
-const saveUpcomingMatches = (upcomingMatches: string): void => {
+// Return the live matches currently in the live.json file. If the file does not exist it is initialized with an empty list. 
+const fetchPreviousLiveMatches = (): unknown => {
+    const file_path = "./data/scheduler/live.json";
+    if (!fs.existsSync(file_path)) {
+        fs.writeFile("./data/scheduler/live.json", JSON.stringify([]), err => {
+            if (err) throw err;
+        });
+    }
+    else {
+        return JSON.parse(fs.readFileSync(file_path, "utf-8"));
+    }
+};
+
+const saveLiveMatches = (upcomingMatches: string): void => {
     const dir = "./data/scheduler";
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
@@ -27,4 +50,4 @@ const saveUpcomingMatches = (upcomingMatches: string): void => {
     });
 };
 
-export { addUpcomingMatches };
+export { addLiveMatches };
