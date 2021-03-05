ed3173c60288238a4b356538d0617b6a04e6d514
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-05 16:21:23 +0100

    Added example code for hltv package.

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import fs = require("fs");
-import { getHighlights, getDuration } from "./services/highlights";
+// import fs = require("fs");
+//import { getHighlights, getDuration } from "./services/highlighter";
+import { addUpcomingMatches } from "./services/scheduler";
 
-const demo = fs.readFileSync("data/demos/gambit-vs-virtus-pro-m1-vertigo.dem");
 
-void getDuration(demo).then(duration => console.log(duration));
-void getHighlights(demo).then(highlights => console.log(highlights));
+addUpcomingMatches();
+// const demo = fs.readFileSync("data/demos/gambit-vs-virtus-pro-m1-vertigo.dem");
+// void getDuration(demo).then(duration => console.log(duration));
+// void getHighlights(demo).then(highlights => console.log(highlights));
--- services/scheduler.ts
+++ services/scheduler.ts
@@ -1,3 +1,12 @@
 // TODO: Add a function that looks at upcoming matches for a given time period and saves all matches that should be processed.
 // It should only add matches that have atleast one star.
 // TODO: The function should use HLTV.getMatches and save the ID and date to a JSON file. 
+import HLTV from 'hltv';
+
+const addUpcomingMatches = (): void => {
+    void HLTV.getMatches().then(res => {
+        console.log(res);
+    });
+};
+
+export { addUpcomingMatches };
