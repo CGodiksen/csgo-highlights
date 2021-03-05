971022c0f398676db05540cdc4f1968935393d74
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-05 17:15:20 +0100

    Added code to save the relevant upcoming matches to persistant storage.

--- services/scheduler.ts
+++ services/scheduler.ts
@@ -2,14 +2,23 @@
 // It should only add matches that have atleast one star.
 // TODO: The function should use HLTV.getMatches and save the ID and date to a JSON file. 
 import HLTV from 'hltv';
+import fs = require("fs");
 
 const addUpcomingMatches = (dayLookhead: number): void => {
     void HLTV.getMatches().then(res => {
         // The limit for how far we look ahead when parsing upcoming matches.
         const epochLimit = Date.now() + (dayLookhead * 24 * 60 * 60 * 1000);
-        
+
         const relevantMatches = res.filter(match => !match.live && match.date && match.date < epochLimit && match.stars > 0);
-        console.log(relevantMatches);
+
+        const dir = "./data";
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        fs.appendFile("./data/upcoming.json", JSON.stringify(relevantMatches), err => {
+            if (err) throw err;
+        });
     });
 };
 
