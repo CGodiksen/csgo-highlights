ed61b3cb1a14e750f92f40d20fd9e3251ca64f9a
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-06 13:48:19 +0100

    Added skeleton for functions to find the exact time the game started in the VOD.

--- services/downloader.ts
+++ services/downloader.ts
@@ -1,24 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 // TODO: Add a function that can retrieve a demo file based on a match id
 // TODO: Add a function that can retrieve all vods of the match based on a match id.
+// TODO: Add a function that first retrieves the first frame and finds when the game actually started in the vod.
 // TODO: The vods should be faily precise so they start exactly at 00:20 or 00:00 on round 1.
 require('dotenv');
 import vision from '@google-cloud/vision';
 //import HLTV from 'hltv';
 
 const downloadDemo = (): void => {
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
+    console.log();
+};
 
-    /**
-     * TODO(developer): Uncomment the following line before running the sample.
-     */
-    const fileName = "./data/test.PNG";
+// Return the exact timestamp of when the game started in the VOD.
+const findGameStart = (): void => {
+    console.log(getRoundTime("./data/test.PNG"));
+};
+
+// Return the time left in the round on a specific frame of the VOD. 
+const getRoundTime = (framePath: string) => {
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();;
 
     // Performs text detection on the local file
-    client.textDetection(fileName).then(result => {
+    client.textDetection(framePath).then(result => {
         const detections = result[0].textAnnotations;
         console.log('Text:');
     
@@ -28,4 +34,4 @@ const downloadDemo = (): void => {
     });
 };
 
-export { downloadDemo };
+export { downloadDemo, findGameStart };
