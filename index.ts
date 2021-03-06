bd432e922d5b479941e69c32de695c709dcf4e10
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-06 11:44:24 +0100

    Added working example of extracting text from image.

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 // import fs = require("fs");
 // import { getHighlights, getDuration } from "./services/highlighter";
-import { startScheduler } from "./services/scheduler";
-require('dotenv');
+// import { startScheduler } from "./services/scheduler";
+import { downloadDemo } from "./services/downloader";
 
-// Adding new upcoming matches once every day and checking if any games are done every 15 minutes. 
-startScheduler(1440, 15, 2);
+// // Adding new upcoming matches once every day and checking if any games are done every 15 minutes. 
+// startScheduler(1440, 15, 2);
 // const demo = fs.readFileSync("data/demos/gambit-vs-virtus-pro-m1-vertigo.dem");
 // void getDuration(demo).then(duration => console.log(duration));
 // void getHighlights(demo).then(highlights => console.log(highlights));
+void downloadDemo();
--- services/downloader.ts
+++ services/downloader.ts
@@ -1,8 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // TODO: Add a function that can retrieve a demo file based on a match id
 // TODO: Add a function that can retrieve all vods of the match based on a match id.
 // TODO: The vods should be faily precise so they start exactly at 00:20 or 00:00 on round 1.
 require('dotenv');
+import vision from '@google-cloud/vision';
 //import HLTV from 'hltv';
 
+const downloadDemo = (): void => {
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
 
-// const downloadDemo
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    const fileName = "./data/test.PNG";
+
+    // Performs text detection on the local file
+    client.textDetection(fileName).then(result => {
+        const detections = result[0].textAnnotations;
+        console.log('Text:');
+    
+        if (detections) {
+            detections.forEach(text => console.log(text));
+        }
+    });
+};
+
+export { downloadDemo };
