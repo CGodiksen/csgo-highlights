d4a2d03fbea3bc8f256b1d9db23cedfcbfa5cf3d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-10 20:30:57 +0100

    Not making highlights from rounds with less than 3 events.

--- services/highlighter.ts
+++ services/highlighter.ts
@@ -21,11 +21,12 @@ const getHighlightSpecification = async (demoFolder: string, demoFile: string): 
     const rounds = splitIntoRounds(moments);
 
     rounds.forEach(round => {
-        // Only adding a highlight if there is more than two actual events (more than bomb plant and bomb explosion).
-        if (round.moments.length > 3) {
-            cleanRoundMoments(round);
+        cleanRoundMoments(round);
+
+        // Only adding a highlight if there is more than two events left after cleaning (more than bomb plant and bomb explosion).
+        if (round.moments.length > 2) {
             console.log(round);
-            
+
             const start = round.moments[0].time - 5;
             const end = round.moments.slice(-1)[0].time + 5;
 
@@ -34,7 +35,7 @@ const getHighlightSpecification = async (demoFolder: string, demoFile: string): 
     });
 
     console.log(`Created a highlight specification with ${highlights.length} highlights from the demo at ${demoFile}`);
-    return { demoFile: demoFile, highlights: highlights};
+    return { demoFile: demoFile, highlights: highlights };
 };
 
 const extractMoments = (demo: Buffer): Promise<Moment[]> => new Promise(resolve => {
@@ -88,7 +89,7 @@ const splitIntoRounds = (moments: Moment[]): Round[] => {
     moments.forEach(moment => {
         if (moment.event === "round_end") {
             rounds.push(round);
-            
+
             roundCounter += 1;
             round = { id: roundCounter, moments: [] };
         } else {
@@ -102,17 +103,19 @@ const splitIntoRounds = (moments: Moment[]): Round[] => {
 // Removing moments from the given round that would decrease the viewing quality of the highlight.
 const cleanRoundMoments = (round: Round): void => {
     round.moments = round.moments.filter(moment => moment.event !== "round_start");
-    
-    // Removing kills that are seperate from the actual highlight of the round.
-    for (let i = 1; i >= 0; i--) {
-        if (round.moments[i].event === "player_death" && (round.moments[i + 1].time - round.moments[i].time) > 30) {
-            round.moments.splice(i, 1);
+
+    if (round.moments.length >= 2) {
+        // Removing kills that are seperate from the actual highlight of the round.
+        for (let i = 1; i >= 0; i--) {
+            if (round.moments[i].event === "player_death" && (round.moments[i + 1].time - round.moments[i].time) > 30) {
+                round.moments.splice(i, 1);
+            }
         }
-    }
-    
-    // Removing the bomb explosion if the CT's are saving and nothing happens between bomb plant and explosion.
-    if (round.moments.slice(-1)[0].event === "bomb_exploded" && round.moments.slice(-2)[0].event === "bomb_planted") {
-        round.moments.splice(-1, 1);
+
+        // Removing the bomb explosion if the CT's are saving and nothing happens between bomb plant and explosion.
+        if (round.moments.slice(-1)[0].event === "bomb_exploded" && round.moments.slice(-2)[0].event === "bomb_planted") {
+            round.moments.splice(-1, 1);
+        }
     }
 };
 
