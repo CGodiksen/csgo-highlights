3b4a6deb1fe4b549a290e1c720565800bc973dd8
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-10 10:56:37 +0100

    Returning the vod path from get related vod.

--- services/editor.ts
+++ services/editor.ts
@@ -5,7 +5,7 @@ import { HighlightSpecification } from "./common/types";
 const createHighlightVideo = (vodFolder: string, hightlightSpecifications: HighlightSpecification[]): void => {
     console.log(vodFolder, hightlightSpecifications);
 
-    const highlightOrderFiles = await Promise.all(hightlightSpecifications.map(spec => ))
+    const highlightOrderFiles = await Promise.all(hightlightSpecifications.map(spec => cutVod()))
     // For each vod do the following concurrently:
         // Create a highlight txt file.
         // Cut out hightlights from the vods.
@@ -15,16 +15,16 @@ const createHighlightVideo = (vodFolder: string, hightlightSpecifications: Highl
     // Return the path to the created hightlight video.
 };
 
-const getRelatedVod = async (vodFolder: string, hightlightSpecification: HighlightSpecification): Promise<string> => {
-    const vodFiles = await glob(`${vodFolder}*.mp4`)
-    const relatedVod = vodFiles.find(file => hightlightSpecification.demoFile.includes(file))
+const getRelatedVodPath = async (vodFolder: string, hightlightSpecification: HighlightSpecification): Promise<string> => {
+    const vodFiles = await glob(`${vodFolder}*.mp4`);
+    const relatedVod = vodFiles.find(file => hightlightSpecification.demoFile.includes(file));
     
     if (relatedVod) {
-        return relatedVod
+        return `${vodFolder}${relatedVod}`;
     } else {
-        throw "Could not find a VOD for the hightlight specification."
+        throw "Could not find a VOD for the hightlight specification.";
     }
-}
+};
 
 // Cut out hightlights from the vod and return a promise to deliver the file path to a txt file specifying the intended order of the clips.
 const cutVod = (vodFilePath: string, hightlightSpecification: HighlightSpecification): void => {
