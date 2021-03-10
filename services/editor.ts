eb072370823c62f623af25cb862a96436abdfca2
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-10 22:44:01 +0100

    Merging order files and fixed minor bug caused by seperator between order files in buffer.

--- services/editor.ts
+++ services/editor.ts
@@ -1,16 +1,16 @@
 import fs from "fs/promises";
 import { HighlightSpecification } from "./common/types";
-import {promiseExec} from "./common/functions";
+import { promiseExec } from "./common/functions";
 
 // Cut the given VODs into a highlight video according to the given hightlight specification.
 const createHighlightVideo = async (vodFolder: string, hightlightSpecifications: HighlightSpecification[]): Promise<void> => {
     console.log(`Creating a hightlight video for VODs in ${vodFolder}...`);
 
     const highlightOrderFiles = await Promise.all(hightlightSpecifications.map(spec => cutVod(vodFolder, spec)));
-    console.log(highlightOrderFiles);
-    
-    // Merge the text files specifying the order of the clips into a single hightlight txt file.
 
+    // Merge the text files specifying the order of the clips into a single hightlight txt file.
+    const mergedOrderFile = await mergeOrderFiles(vodFolder, highlightOrderFiles);
+    console.log(mergedOrderFile);
 
     // Use the ffmpeg concat demuxer method to concatenate all hightlight clips into a single hightlight video.
     // Return the path to the created hightlight video.
@@ -19,10 +19,10 @@ const createHighlightVideo = async (vodFolder: string, hightlightSpecifications:
 // Cut out hightlights from the vod and return a promise to deliver the file path to a txt file specifying the intended order of the clips.
 const cutVod = async (vodFolder: string, hightlightSpec: HighlightSpecification): Promise<string> => {
     console.log(`Cutting the VOD at ${hightlightSpec.vodFilePath!} into ${hightlightSpec.highlights.length} clips...`);
-    
+
     const mapNumber = hightlightSpec.vodFilePath!.slice(-6).slice(0, 2);
     const highlightOrderFilePath = `${vodFolder}${mapNumber}.txt`;
-    
+
     for (const hightlight of hightlightSpec.highlights) {
         const clipFilePath = `${vodFolder}${mapNumber}_${hightlight.roundNumber}.mp4`;
         await promiseExec(`ffmpeg -ss ${hightlight.start} -i ${hightlightSpec.vodFilePath!} -to ${hightlight.duration} -c copy ${clipFilePath}`);
@@ -35,10 +35,10 @@ const cutVod = async (vodFolder: string, hightlightSpec: HighlightSpecification)
 
 // Return a promise to deliver the file path to the merged order file when done merging.
 const mergeOrderFiles = async (vodFolder: string, orderFiles: string[]): Promise<string> => {
-    const mergedFilePath = `${vodFolder}merged.txt`
+    const mergedFilePath = `${vodFolder}merged.txt`;
     const content = await Promise.all(orderFiles.map(orderFile => fs.readFile(orderFile)));
 
-    await fs.writeFile(mergedFilePath, content.toString());
+    await fs.writeFile(mergedFilePath, content.toString().replace(",", ""));
     return mergedFilePath;
 };
 
