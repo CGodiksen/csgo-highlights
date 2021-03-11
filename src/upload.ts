57433ed065afd76357e398ebeb447433fd94b8d8
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-11 16:23:49 +0100

    Adding logging to upload.

--- src/upload.ts
+++ src/upload.ts
@@ -3,8 +3,12 @@ import { FullMatch } from "hltv/lib/models/FullMatch";
 
 // "Upload" the video by saving it to the specified location under a match specific name.
 const uploadHighlightVideo = async (videoPath: string, match: FullMatch, savePath: string): Promise<void> => {
+    
     const title = createTitle(match);
-    await fs.rename(videoPath, `${savePath}${title.replace(/[&/\\#,+()$~%.'":*?<>{}]/g, "")}.mp4`);
+    const filePath = `${savePath}${title.replace(/[&/\\#,+()$~%.'":*?<>{}]/g, "")}.mp4`;
+
+    await fs.rename(videoPath, filePath);
+    console.log(`"Uploaded" the highlight video at ${videoPath} to ${filePath}`);
 };
 
 const createTitle = (match: FullMatch) => {
