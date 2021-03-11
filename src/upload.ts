e85b5773b316009b18f80baed8fcc8c15d7b91f5
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-11 10:05:04 +0100

    Added module to supporting uploading the highlight video.

---
+++ src/upload.ts
@@ -0,0 +1,7 @@
+import { FullMatch } from "hltv/lib/models/FullMatch";
+
+const uploadHighlightVideo = (videoPath: string, match: FullMatch): void => {
+    console.log(videoPath, match);
+};
+
+export {uploadHighlightVideo};
