3edffa65e909f23f50546e928aa679dd65819b33
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-14 21:53:28 +0100

    Added simple function to upload to youtube.

--- src/upload.ts
+++ src/upload.ts
@@ -1,5 +1,16 @@
-import fs from "fs/promises";
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import path from 'path';
+import readline from 'readline';
+import { google } from 'googleapis';
+import { authenticate } from '@google-cloud/local-auth';
+import fs from "fs";
 import { FullMatch } from "hltv/lib/models/FullMatch";
+
+// initialize the Youtube API library
+const youtube = google.youtube('v3');
 
 // "Upload" the video by saving it to the specified location under a match specific name.
 const uploadHighlightVideo = async (videoPath: string, match: FullMatch, savePath: string): Promise<void> => {
@@ -8,7 +19,7 @@ const uploadHighlightVideo = async (videoPath: string, match: FullMatch, savePat
     const filePath = `${savePath}${title.replace(/[&/\\#,+()$~%.'":*?<>{}]/g, "")}.mp4`;
 
     try {
-        await fs.rename(videoPath, filePath);
+        fs.renameSync(videoPath, filePath);
         console.log(`"Uploaded" the highlight video at ${videoPath} to ${filePath}`);
     } catch (uploadError) {
         console.error(uploadError);
@@ -18,5 +29,40 @@ const uploadHighlightVideo = async (videoPath: string, match: FullMatch, savePat
 const createTitle = (match: FullMatch) => {
     return `${match.team1!.name} vs ${match.team2!.name} - ${match.event.name}`;
 };
+
+// very basic example of uploading a video to youtube
+const upload = async (fileName: string) => {
+    // Obtain user credentials to use for the request
+    const auth = await authenticate({
+        keyfilePath: path.join(__dirname, '../config/client_secret.json'),
+        scopes: [
+            'https://www.googleapis.com/auth/youtube.upload',
+            'https://www.googleapis.com/auth/youtube',
+        ],
+    });
+    google.options({ auth });
+
+    const res = await youtube.videos.insert(
+        {
+            part: ['id,snippet,status'],
+            notifySubscribers: false,
+            requestBody: {
+                snippet: {
+                    title: 'Node.js YouTube Upload Test',
+                    description: 'Testing YouTube upload via Google APIs Node.js Client',
+                },
+                status: {
+                    privacyStatus: 'private',
+                },
+            },
+            media: {
+                body: fs.createReadStream(fileName),
+            },
+        }
+    );
+    console.log(res.data);
+    return res.data;
+};
+
 
 export { uploadHighlightVideo };
